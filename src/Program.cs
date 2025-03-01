aa69fbb4fcdab2369895697e1e836795129674c5
Tim Hanewich <tahanewich@live.com>
2025-03-01 10:38:24 -0500

    Set up test

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AgentFramework
 {
@@ -6,8 +10,46 @@ namespace AgentFramework
     {
         public static void Main(string[] args)
         {
-            Tool t = new Tool("search_web", "Search the world wide web for information about a particular topic.");
-            t.Parameters.Add(new ToolInputParameter("subject", "The term to search for."));
+            TestAsync().Wait();
+        }
+
+        public static async Task TestAsync()
+        {
+            Agent a = new Agent();
+            a.Credentials = JsonConvert.DeserializeObject<AzureOpenAICredentials>(System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\AgentFramework\credentials.json"));
+
+            //messages
+            a.Messages.Add(new Message(Role.system, "You are a helpful assistant."));
+            a.Messages.Add(new Message(Role.user, "What is the temperature outside in Sarasota, Florida?"));
+
+            //tools
+            Tool tool = new Tool("check_temperature", "Check the temperature for a given location.");
+            tool.Parameters.Add(new ToolInputParameter("location", "The name of the location you want to check the temperature of, i.e. 'Seattle, WA' or 'Atlanta, GA'."));
+            a.Tools.Add(tool);
+
+            //Add tool call message
+            Message tcm = new Message();
+            tcm.Role = Role.assistant;
+            ToolCall tc = new ToolCall();
+            tc.ToolName = "check_temperature";
+            tc.Arguments = new JObject();
+            tc.Arguments.Add("location", "Sarasota, Florida");
+            tc.ID = "call_KxLw58XlPkmk31fumktijVqr";
+            tcm.ToolCalls = new ToolCall[]{tc};
+            a.Messages.Add(tcm);
+
+            //Add tool call response
+            Message tcr = new Message();
+            tcr.ToolCallID = "call_KxLw58XlPkmk31fumktijVqr";
+            tcr.Role = Role.tool;
+            tcr.Content = "Temperature in Sarasota, Florida: 72.5 degrees F";
+            a.Messages.Add(tcr);
+
+            Message resp = await a.PromptAsync();
+
+            Console.WriteLine(JsonConvert.SerializeObject(resp));
+
+
         }
     }
 }
