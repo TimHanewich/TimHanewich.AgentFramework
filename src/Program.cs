b2ba16ae4d8349b40a33fb2c5650e4e11203698b
Tim Hanewich <tahanewich@live.com>
2025-03-01 10:45:29 -0500

    Created test

--- src/Program.cs
+++ src/Program.cs
@@ -18,36 +18,50 @@ namespace AgentFramework
             Agent a = new Agent();
             a.Credentials = JsonConvert.DeserializeObject<AzureOpenAICredentials>(System.IO.File.ReadAllText(@"C:\Users\timh\Downloads\AgentFramework\credentials.json"));
 
-            //messages
+            //Add system message
             a.Messages.Add(new Message(Role.system, "You are a helpful assistant."));
-            a.Messages.Add(new Message(Role.user, "What is the temperature outside in Sarasota, Florida?"));
 
-            //tools
+            
+
+            //Add tool: check weather
             Tool tool = new Tool("check_temperature", "Check the temperature for a given location.");
             tool.Parameters.Add(new ToolInputParameter("location", "The name of the location you want to check the temperature of, i.e. 'Seattle, WA' or 'Atlanta, GA'."));
             a.Tools.Add(tool);
 
-            //Add tool call message
-            Message tcm = new Message();
-            tcm.Role = Role.assistant;
-            ToolCall tc = new ToolCall();
-            tc.ToolName = "check_temperature";
-            tc.Arguments = new JObject();
-            tc.Arguments.Add("location", "Sarasota, Florida");
-            tc.ID = "call_KxLw58XlPkmk31fumktijVqr";
-            tcm.ToolCalls = new ToolCall[]{tc};
-            a.Messages.Add(tcm);
 
-            //Add tool call response
-            Message tcr = new Message();
-            tcr.ToolCallID = "call_KxLw58XlPkmk31fumktijVqr";
-            tcr.Role = Role.tool;
-            tcr.Content = "Temperature in Sarasota, Florida: 72.5 degrees F";
-            a.Messages.Add(tcr);
+            //Add welcoming message
+            string opening_msg = "Hello! I'm here to help. What can I do for you?";
+            a.Messages.Add(new Message(Role.assistant, opening_msg));
+            Console.WriteLine(opening_msg);
 
-            Message resp = await a.PromptAsync();
+            while (true)
+            {
+                //Collect input
+                Console.WriteLine();
+                string? input = null;
+                while (input == null)
+                {
+                    Console.Write("> ");
+                    input = Console.ReadLine();
+                }
 
-            Console.WriteLine(JsonConvert.SerializeObject(resp));
+                //Append message
+                a.Messages.Add(new Message(Role.user, input));
+
+                //Prompt
+                Console.WriteLine();
+                Console.Write("Thinking...");
+                Message response = await a.PromptAsync();
+
+                //Write
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(response.Content);
+
+                //Add to message array
+                a.Messages.Add(response);
+            }
+            
 
 
         }
