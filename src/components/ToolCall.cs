3b92daf7d5fa36dd27f60bd93bf2fc557e3c343c
Tim Hanewich <tahanewich@live.com>
2025-03-01 08:36:13 -0500

    Parsing of tool calls

--- src/components/ToolCall.cs
+++ src/components/ToolCall.cs
@@ -9,5 +9,43 @@ namespace AgentFramework
         public string ToolName {get; set;}
         public JObject Arguments {get; set;}
         public string ID {get; set;}
+
+        public ToolCall()
+        {
+            ToolName = "";
+            Arguments = new JObject();
+            ID = "";
+        }
+
+        public static ToolCall Parse(JObject tool_call)
+        {
+            ToolCall ToReturn = new ToolCall();
+
+            //Get tool name
+            JToken? name = tool_call.SelectToken("function.name");
+            if (name != null)
+            {
+                ToReturn.ToolName = name.ToString();
+            }
+
+            //Get arguments
+            JToken? arguments = tool_call.SelectToken("function.arguments");
+            if (arguments != null)
+            {
+                string arguments_json = arguments.ToString();
+                ToReturn.Arguments = JObject.Parse(arguments_json);
+            }
+
+            //get tool call ID
+            JProperty? id = tool_call.Property("id");
+            if (id != null)
+            {
+                ToReturn.ID = id.Value.ToString();
+            }
+
+            return ToReturn;
+        }
+
+
     }
 }
