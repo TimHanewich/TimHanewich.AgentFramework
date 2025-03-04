520815b39699192ae1c98dc53a8ae20106474365
Tim Hanewich <tahanewich@live.com>
2025-03-03 20:43:47 -0500

    Set up test

---
+++ src/testing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TimHanewich.AgentFramework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AgentFrameworkTesting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TimHanewich.AgentFramework.OllamaModel om = new TimHanewich.AgentFramework.OllamaModel();
+            om.ModelIdentifier = "llama3.2:3b";
+
+            Agent a = new Agent();
+            a.Model = om;
+            a.Messages.Add(new Message(Role.system, "You are a helpful assistant."));
+
+
+            a.Tools.Add(new Tool("check_temperature", "Check the current temperature."));
+
+
+
+            a.Messages.Add(new Message(Role.user, "What is the temperature?"));
+            Console.WriteLine("Calling...");
+            Message m = a.PromptAsync().Result;
+            a.Messages.Add(m);
+
+            Console.WriteLine(JsonConvert.SerializeObject(a, Formatting.Indented));
+        }
+    }
+}
