1ddedaeba462a6de19054294953fa0f239d4dcd3
Tim Hanewich <tahanewich@live.com>
2025-03-07 11:49:10 -0500

    Message array length can now be specified

--- src/Agent.cs
+++ src/Agent.cs
@@ -7,6 +7,8 @@ using System.Net.Http;
 using Newtonsoft.Json.Linq;
 using System.Text;
 using System.Text.Encodings;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace TimHanewich.AgentFramework
 {
@@ -41,15 +43,60 @@ namespace TimHanewich.AgentFramework
         }
 
         //Ask model to generate the next message, given the current context of messages
-        public async Task<Message> PromptAsync()
+        public async Task<Message> PromptAsync(int past_messages_included = 10)
         {
+            //Validate that there is a model
             if (Model == null)
             {
                 throw new Exception("Unable to prompt any model as you did not provide a model connection of any kind to this agent!");
             }
 
+            //Validate that there is at least one message
+            if (Messages.Count == 0)
+            {
+                throw new Exception("Unable to prompt model: You must have add at least one message before prompting the model!");
+            }
+
+            //Validate that they want at least one past message
+            if (past_messages_included <= 0)
+            {
+                throw new Exception("You specified '" + past_messages_included.ToString() + "' past messaged to be included in the prompt to the model. This must be greater than 0.");
+            }
+
+            //Collect the correct number of messages
+            List<Message> MessagesToPrompt = new List<Message>();
+            for (int i = Messages.Count - 1; i >= 0; i--)
+            {
+                if (MessagesToPrompt.Count < past_messages_included)
+                {
+                    MessagesToPrompt.Add(Messages[i]); //Add
+                }
+            }
+            MessagesToPrompt.Reverse(); //Flip the order (since we just added them in the opposite order)
+
+            //Find system message... ensure at least that is included
+            Message? SystemMessage = null;
+            foreach (Message msg in Messages)
+            {
+                if (SystemMessage == null)
+                {
+                    if (msg.Role == Role.system)
+                    {
+                        SystemMessage = msg;
+                    }
+                }
+            }   
+            
+            //If there indeed was a system message, replace the first message with that system message
+            // (it may already be in there as the first item, but just replacing it anyway is fine)
+            if (SystemMessage != null)
+            {
+                MessagesToPrompt[0] = SystemMessage;
+            }
+
             //Invoke the inference via whatever model they provided!
-            InferenceResponse ir = await Model.InvokeInferenceAsync(Messages.ToArray(), Tools.ToArray());
+            Console.WriteLine(JsonConvert.SerializeObject(MessagesToPrompt, Formatting.Indented));
+            InferenceResponse ir = await Model.InvokeInferenceAsync(MessagesToPrompt.ToArray(), Tools.ToArray());
 
             //Increment token consumptions
             _CumulativePromptTokens = _CumulativePromptTokens + ir.PromptTokensConsumed;
