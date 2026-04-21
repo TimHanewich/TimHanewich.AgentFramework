a4c42a75ed781cba1e62eb241e7755d0f44d7a8b
Tim Hanewich <tahanewich@live.com>
2026-04-20 20:04:41 -0400

    Cleared out aold test

--- testing/Program.cs
+++ testing/Program.cs
@@ -9,23 +9,7 @@ namespace AgentFrameworkTesting
     {
         public static void Main(string[] args)
         {
-            TimHanewich.AgentFramework.OllamaModel om = new TimHanewich.AgentFramework.OllamaModel();
-            om.ModelIdentifier = "qwen2.5:7b";
-
-            Agent a = new Agent();
-            a.Model = om;
-            a.Messages.Add(new Message(Role.system, "You are a helpful assistant."));
-
-
-            a.Tools.Add(new Tool("check_temperature", "Check the current temperature."));
-
-
-
-            a.Messages.Add(new Message(Role.user, "What is the temperature outside?"));
-            Console.WriteLine("Calling...");
-            Message m = a.PromptAsync().Result;
-
-            Console.WriteLine(JsonConvert.SerializeObject(m, Formatting.Indented));
+            
         }
     }
 }
