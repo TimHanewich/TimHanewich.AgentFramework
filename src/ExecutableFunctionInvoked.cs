09f4ffba9d654292cb09e0dd85ec8508a9dfdbca
Tim Hanewich <tahanewich@live.com>
2026-04-23 07:16:49 -0400

    Renamed

--- src/ExecutableFunctionInvoked.cs
+++ src/ExecutableFunctionInvoked.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json.Linq;
 
 namespace TimHanewich.AgentFramework
 {
-    public delegate void ExecutableFunctionAction(ExecutableFunction function, JObject arguments);
+    public delegate void ExecutableFunctionHandler(ExecutableFunction function, JObject arguments);
 
     public delegate void TokenUsageHandler(int input_tokens, int output_tokens);
 }
